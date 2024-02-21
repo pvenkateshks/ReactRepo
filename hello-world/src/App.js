cbb91aea25a375b8a7b43c6fb764718e75ddf08a
pvenkateshks <priyankavenkatesh196@gmail.com>
2024-02-21 11:45:45 +0530

    added Functional Component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+let name = "Priyanka";
+
+    function App(){
+      return(
+        <>
+        <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        </nav>
+        <div className="container">
+          <h1>Hello {name}</h1>
+        </div>
+        </>
   );
 }
-
 export default App;
